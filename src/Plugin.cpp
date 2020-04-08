fb07bae0cd12abd6b3e72df44fb58f935e43f942
TheNiteCoder <coderofthenite@gmail.com>
2020-04-08 11:26:04 -0400

    rolling back i18n support that crashes plugin

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -66,6 +66,6 @@ namespace Plugin
 
 extern "C" void construct(XfcePanelPlugin* xfPlugin)
 {
-	xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
+//	xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 	Plugin::init(xfPlugin);
 }
