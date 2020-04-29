feb31e2e39072d4943bcc699c547fb5358e72c56
TheNiteCoder <coderofthenite@gmail.com>
2020-04-28 21:42:54 -0400

    Ported over the add AppInfo support from upstream nsz32/docklike-plugin

--- src/AppInfos.hpp
+++ src/AppInfos.hpp
@@ -3,12 +3,20 @@
 #ifndef APPINFOS_HPP
 #define APPINFOS_HPP
 
+#include <pthread.h>
+#include <sys/inotify.h>
+
 #include <iostream>
 
 #include "Helpers.hpp"
 #include "Store.tpp"
 
-struct AppInfo { const std::string path; const std::string icon; const std::string name; };
+struct AppInfo
+{
+	const std::string path;
+	const std::string icon;
+	const std::string name;
+};
 
 namespace AppInfos
 {
@@ -16,6 +24,6 @@ namespace AppInfos
 
 	AppInfo* search(std::string id);
 	void launch(AppInfo* appInfo);
-}
+} // namespace AppInfos
 
 #endif
